cdfa0e377b48927b3e54c945b966fa683fdfb738
kyblie <35406490+kyblie@users.noreply.github.com>
2022-03-18 17:45:42 -0600

    Update ENEL-400-Autonomous-Rover.ino

--- ENEL-400-Autonomous-Rover.ino
+++ ENEL-400-Autonomous-Rover.ino
@@ -19,11 +19,14 @@ void loop() {
 
   //moving straight forward at full speed when the front is clear of obstacles
   if( Lsensor.distCM >= 100 && Rsensor.distCM >= 100)  {
-    move(220);
+    move(160);
   }
-  //moving straight foward at half speed when there are obstacles nearby
   else if( Lsensor.distCM >= 30 && Rsensor.distCM >= 30)  {
-    move(110);
+    //moving straight foward at half speed when there are obstacles nearby
+    //move(80);
+
+    //avoid obstacles
+    move(100, true, (Rsensor.distCM - Lsensor.distCM) / 500);
   }
   else  {   //stop when an obstacle is closed
     mbrake;
