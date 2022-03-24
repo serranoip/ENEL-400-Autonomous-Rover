e9c715c3f23c39115eb1018d7277b5fad75e3525
vtdau <77660884+vtdau@users.noreply.github.com>
2022-03-24 12:39:26 -0600

    ENEL400_FinalProject
    Updated code for Show Day April 5

---
+++ ENEL400_FinalProject/a_global.ino
@@ -0,0 +1,9 @@
+//declare library
+//declare pin locations
+//declare parameter
+
+#include <Wire.h>                            // Wire library require for I2C protocol
+#include <I2Cdev.h>                          // Include I2C protocol
+#include <MPU6050.h>                         // Include Gyrometer
+
+int trigPin;
