76c54c3ce564d2fab0a7afc8b31904ba1b462fd0
vtdau <77660884+vtdau@users.noreply.github.com>
2022-03-24 16:07:26 -0600

    Add files via upload

--- ENEL400_FinalProject/x_setup.ino
+++ ENEL400_FinalProject/x_setup.ino
@@ -1,1 +1,21 @@
+void setup()  {
+  Serial.begin(9600);
 
+  // RIGHT sensor //
+  pinMode(trigPinR, OUTPUT);
+  pinMode(echoPinR, INPUT);  
+
+  // LEFT sensor
+  pinMode(trigPinL, OUTPUT);
+  pinMode(echoPinL, INPUT);
+
+  // Motor on the Right
+  pinMode(speedPinR, OUTPUT);
+  pinMode(dir1R, INPUT);
+  pinMode(dir2R, INPUT);
+
+  // Motor on the Left
+  pinMode(speedPinL, OUTPUT);
+  pinMode(dir1L, INPUT);
+  pinMode(dir2L, INPUT);
+}
