5ac1aea2bd565cc1e72c57aeb6d33e129c017221
vtdau <77660884+vtdau@users.noreply.github.com>
2022-03-24 15:54:54 -0600

    Updated ENEL400 code
    adding ultrasonic sensor vs motor action

---
+++ ENEL400_FinalProject/a_global/c_motor.ino
@@ -0,0 +1,87 @@
+//start and stop
+
+class motor
+{
+  public:
+    void powerUp()
+    {
+      digitalWrite(dir1R, LOW);  
+      digitalWrite(dir2R, LOW);
+      digitalWrite(dir1L, LOW);
+      digitalWrite(dir2L, LOW);
+
+      digitalWrite(dir1R, HIGH); 
+      delay(10);
+      digitalWrite(dir1R, LOW);
+      delay(10);
+      digitalWrite(dir1L, HIGH); 
+      delay(10);
+      digitalWrite(dir1L, LOW);
+      delay(10);
+    }
+    
+    void fullStop() //full stop
+    {
+      digitalWrite(dir1R, LOW);  
+      digitalWrite(dir2R, LOW);
+      digitalWrite(dir1L, LOW);
+      digitalWrite(dir2L, LOW);
+    }
+  
+    void forwardSpeed(int velocity) //forward at input speed value //minimum velocity 0 and maximum at 255
+    {
+      digitalWrite(dir1R, HIGH);  
+      digitalWrite(dir2R, LOW);
+      digitalWrite(dir1L, HIGH);
+      digitalWrite(dir2L, LOW);
+      analogWrite(speedPinR, velocity);
+      analogWrite(speedPinL, velocity);
+    }
+  
+    void backwardSpeed(int velocity)  //go backward
+    {
+      digitalWrite(dir1R, LOW);  
+      digitalWrite(dir2R, HIGH);
+      digitalWrite(dir1L, LOW);
+      digitalWrite(dir2L, HIGH);
+      analogWrite(speedPinR, velocity);
+      analogWrite(speedPinL, velocity);
+    }
+  
+    void right() //Turns car to the right
+    {
+      digitalWrite(dir1R, HIGH);  
+      digitalWrite(dir2R, LOW);
+      digitalWrite(dir1L, LOW);
+      digitalWrite(dir2L, LOW);
+    }
+  
+    void left() //Turns car to the left
+    {
+      digitalWrite(dir1R, LOW);  
+      digitalWrite(dir2R, LOW);
+      digitalWrite(dir1L, HIGH);
+      digitalWrite(dir2L, LOW);
+    }
+  
+    void adjustRight() // turn the car slightly to the right
+      {
+        digitalWrite(dir1L, LOW);
+        delay(1);
+        digitalWrite(dir1L, HIGH);
+      }
+      
+    void adjustLeft() // turn the car slightly to the right
+      {
+        digitalWrite(dir1R, LOW);
+        delay(1);
+        digitalWrite(dir1R, HIGH);
+      }
+
+  
+
+  
+  
+  private:
+    
+};
