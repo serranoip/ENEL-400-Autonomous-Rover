eaeebd547fa52d4f580b7951697366ccb319d0ec
vtdau <77660884+vtdau@users.noreply.github.com>
2022-03-24 12:37:19 -0600

    Delete a_global.ino

--- a_global.ino
+++
@@ -1,9 +0,0 @@
-//declare library
-//declare pin locations
-//declare parameter
-
-#include <Wire.h>                            // Wire library require for I2C protocol
-#include <I2Cdev.h>                          // Include I2C protocol
-#include <MPU6050.h>                         // Include Gyrometer
-
-int trigPin;
