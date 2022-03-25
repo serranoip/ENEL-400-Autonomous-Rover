dfe43b00fbc844b22dba4c4fe9a4af9f73393bef
vtdau <77660884+vtdau@users.noreply.github.com>
2022-03-25 14:16:38 -0600

    Add files via upload

--- ENEL400_FinalProject/e_roverAction.ino
+++ ENEL400_FinalProject/e_roverAction.ino
@@ -15,20 +15,28 @@ class roverAction
 
       if(dR > 50 && dL >50) {
         motion.forwardSpeed(150);
+        //gyro to keep it go straight
+        //having adjustLeft, adjustRight code
+        
       }
       else if(dR > 20 && dL > 20) {
         motion.forwardSpeed(75);
+        //same gyro code to keep it go straight
       }
       else if(dR < 20)  {
         motion.left();
+        //gyro to turn it back on track 
       }
       else if(dL < 20)  {
         motion.right();
+        //gyro to turn it back on track
       }
-      else if(dR < 10 && dL < 10) {
+      else if(dR < 7 && dL < 7) {
         motion.fullStop();
+        //wait 5 second and scan again 
+        //if still less than 7 cm 
+        //turn to the side
       }
-      
     }
 
   private:
