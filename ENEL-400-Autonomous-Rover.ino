4260f7fb43f8a0047295e53600a10cd919e9536c
kyblie <35406490+kyblie@users.noreply.github.com>
2022-04-04 20:03:29 -0600

    I think it works

--- ENEL-400-Autonomous-Rover.ino
+++ ENEL-400-Autonomous-Rover.ino
@@ -1,12 +1,17 @@
 #include <NewPing.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
-//#include "ultrasonic.h"
 #include "motor.h"
 #define TRIGGER_PIN_LEFT 12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN_LEFT    39
 #define TRIGGER_PIN_RIGHT   4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN_RIGHT    36  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE     100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+const char *ssid = "esp32";
+WiFiServer server(80);
 
 NewPing Rsensor(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
 NewPing Lsensor(TRIGGER_PIN_LEFT,  ECHO_PIN_LEFT,  MAX_DISTANCE);
@@ -15,18 +20,28 @@ int distR;
 int distL;
 int lastscan;
 
-int valid_distR;
-int valid_distL;
+bool driving = false;
+
 void setup() {
-  // put your setup code here, to run once:
-  
   Serial.begin(115200);
   motorSetup();
 
+  pinMode(17, OUTPUT); //solenoid pin
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  // You can remove the password parameter if you want the AP to be open.
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+  Serial.println("Server started");
+  
   lastscan = millis();
 }
 
-void loop() {
+void drive() {
   delay(50);
   lastscan = millis();
   distL = Lsensor.ping_cm(); 
@@ -62,4 +77,68 @@ void loop() {
     Serial.printf("speed: %d, diff: %f\n", speed, 0.0);
   }
 }
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<p style=\"font-size:60px;\">");
+            client.print("Click <a href=\"/D\">here</a> to turn Navigate.<br>");
+            client.print("Click <a href=\"/S\">here</a> to turn Stop.<br><br>");
+            client.print("Click <a href=\"/H\">here</a> to turn Unlock.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn Lock.<br>");
+            client.print("</p>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(17, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(17, LOW);                // GET /L turns the LED off
+        }
+        if (currentLine.endsWith("GET /D")) {
+          driving = true;             // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /S")) {
+          driving = false;
+          mbrake();
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
   
+  if (driving){
+    drive();
+  }
+}
